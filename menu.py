aff2407f76885bf4166013ee0a15f8a860f7f244
burufeiba <hejinhj@foxmail.com>
2014-06-27 05:19:27 +0800

    1. 重制翻译算法，微调程序界面 2. 大幅优化字典压制和搜索效率，解决了用外部变量做数据库表名的问题。

--- menu.py
+++ menu.py
@@ -3,11 +3,8 @@
 
 import wx
 import sys
-import pickle
 import scripts
-import mysql
-
-CODING = sys.getfilesystemencoding()
+import mydict
 
 """
     =======================================================
@@ -24,6 +21,7 @@ class MyApp(wx.App):
     def OnInit(self):
         self.frame = MyFrame(None)
         self.frame.Show()
+        self.frame.load(wx.EVT_BUTTON)
         return True
 
 
@@ -34,12 +32,16 @@ class MyFrame(wx.Frame):
 
         panel = wx.Panel(self)
         btn_load = wx.Button(panel, label='Load')
-        btn_skip = wx.Button(panel, label='Skip')
         btn_space = wx.Button(panel, label='Add space')
         btn_save = wx.Button(panel, label='Save')
         btn_store = wx.Button(panel, label='Store')
 
         self.file_name = wx.TextCtrl(panel, value='uimain.csv')
+        self.list_en = wx.ListCtrl(panel, style=wx.LC_REPORT| 
+                                            wx.LC_HRULES| 
+                                                wx.LC_VRULES)
+        self.list_en.InsertColumn(0, 'id', format=wx.LIST_FORMAT_CENTER)
+        self.list_en.InsertColumn(1, 'source', format=wx.LIST_FORMAT_LEFT)
         self.contents_en = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
         self.contents_cn = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
         self.contents_cn_space = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
@@ -51,10 +53,10 @@ class MyFrame(wx.Frame):
         hbox0.Add(btn_load, proportion=0, flag=wx.EXPAND | wx.LEFT, border=5)
 
         hbox1 = wx.BoxSizer()
-        hbox1.Add(self.contents_en, proportion=1, flag=wx.EXPAND)
-        hbox1.Add(btn_skip, proportion=0, flag=wx.EXPAND | wx.LEFT, border=5)
+        hbox1.Add(self.list_en, proportion=1, flag=wx.EXPAND)
 
         hbox2 = wx.BoxSizer()
+        hbox2.Add(self.contents_en, proportion=1, flag=wx.EXPAND)
         hbox2.Add(self.contents_cn, proportion=1, flag=wx.EXPAND)
         hbox2.Add(btn_space, proportion=0, flag=wx.EXPAND | wx.LEFT, border=5)
 
@@ -76,92 +78,49 @@ class MyFrame(wx.Frame):
         panel.SetSizer(vbox)
 
         btn_load.Bind(wx.EVT_BUTTON, self.load)
-        btn_skip.Bind(wx.EVT_BUTTON, self.skip)
         btn_space.Bind(wx.EVT_BUTTON, self.addspace)
         btn_store.Bind(wx.EVT_BUTTON, self.store)
         btn_save.Bind(wx.EVT_BUTTON, self.save)
+        self.list_en.Bind(wx.EVT_LIST_ITEM_SELECTED, self.on_item_selected)
 
     def load(self, event):
-        self.current_translation = []
-        self.f_en = open(self.file_name.GetValue(), 'U')
-        with open('offset.txt', 'a+') as f_offset:
-            self.offset = scripts.get_offset(f_offset)
-            self.f_en.seek(self.offset)
-        self.nextline(wx.EVT_BUTTON)
-        self.print_stored('Welcome!'.center(69, '='))
-        self.print_log(
-            '{0}>>>LOAD:{1}'.format(scripts.cur_time(),
-                                     self.file_name.GetValue()))
+        with open(self.file_name.GetValue(), 'U') as source_file:
+            self.source_list = source_file.readlines()
+        for col, source in enumerate(self.source_list):
+            source = source.decode(scripts.CODING)
+            index = self.list_en.InsertStringItem(sys.maxint, str(col))
+            self.list_en.SetStringItem(index, 1, source)
+        self.clear_cn()
 
-    def nextline(self, event):
-        self.source = self.f_en.readline().decode(CODING)
-        self.source_txt = scripts.get_source_txt(self.source)
+    def on_item_selected(self, event):
+        self.index = event.GetIndex()
+        item = self.list_en.GetItem(self.index, 1)
+        source = item.GetText()
+        self.source_txt = scripts.get_source_txt(source)
         self.contents_en.SetValue(self.source_txt[1])
-        self.contents_cn.SetValue('')
-        self.contents_cn_space.SetValue('')
+        self.clear_cn()
 
     def addspace(self, event):
-        usr_input = self.contents_cn.GetValue()
+        usr_input = self.contents_cn.GetValue()     #usr_input coding: unicode
         usr_input_space = scripts.add_space(usr_input)
         self.contents_cn_space.SetValue(usr_input_space)
-        if  self.contents_cn.GetValue():
-            self.print_log(
-                '{0}>>>ADD SPACE:{2}'.format(scripts.cur_time(),
-                                             usr_input.encode(CODING),
-                                             usr_input_space.encode(CODING)))
 
     def store(self, event):
         raw_translation = self.contents_cn_space.GetValue()
-        translation = scripts.get_translation(self.source_txt,
-            raw_translation)
-        if translation not in self.current_translation:
-            self.current_translation.append(translation)
-            if translation:
-                self.print_stored(translation.strip('\n'))
-                self.offset = self.f_en.tell()
-                self.nextline(wx.EVT_BUTTON)
-                self.print_log(
-                    '{0}>>>STORE LINE:{1}'.format(scripts.cur_time(),
-                    translation.strip('\n').encode(CODING)))
-
-    def skip(self, event):
-        with open('undone.csv', 'a') as f_undone:
-            f_undone.write(self.source)
-        self.offset = self.f_en.tell()
-        self.save_offset(self.offset)
-        self.nextline(wx.EVT_BUTTON)
-        self.print_log('{0}>>> SKIP LINE {1}'.format(scripts.cur_time(),
-            self.source.strip('\n')))
+        translation = scripts.get_translation(self.source_txt, raw_translation)
+        if translation:
+            self.source_list[self.index] = translation
+            self.list_en.SetStringItem(self.index, 1, translation)
 
     def save(self, event):
-        if self.current_translation:
-            with open('uimain_cn.csv', 'a') as f_cns:
-                for line in self.current_translation:
-                    f_cns.write(line.encode(CODING))
-                    self.print_log('{0}>>> SAVE CONTENTS: {1}'.format(
-                        scripts.cur_time(), line.strip('\n').encode(CODING)))
-                self.save_offset(self.offset)
-                self.current_translation = []
+        translation_file = open(self.file_name.GetValue(), 'w')
+        for source in self.source_list:
+            translation_file.write(source.encode(scripts.CODING))
+        translation_file.close()
 
-    def save_offset(self, offset):
-        with open('offset.txt', 'w') as f_offset:
-            pickle.dump(offset, f_offset)
-            self.print_log('{0}>>> SAVE OFFSET: {1}'.format(scripts.cur_time(),
-                self.offset))
-
-    def print_stored(self, sth):
-        sys.stdout = self.contents_stored
-        print sth
-
-    def print_log(self, sth):
-        sys.stdout = self.contents_log
-        print sth
-
-    def OnExit(self):
-        self.f_en.close()
-
-
-
+    def clear_cn(self):
+        self.contents_cn.SetValue('')
+        self.contents_cn_space.SetValue('')
 
 if __name__ == '__main__':
     app = MyApp()
--- scripts.py
+++ scripts.py
@@ -2,20 +2,25 @@
 #!usr/bin/env python
 
 import re
-import pickle
-import datetime
+import sys
+# import datetime
+
+CODING = sys.getfilesystemencoding()
+LOGIN = {'host'  :'127.0.0.1',
+         'user':'root',
+         'passwd':'Aa68553^',
+         'db':'translation',
+         'charset':'utf8'}   #must be 'utf8', rather than 'utf-8'
 
 """
-    =============================================================================================###########################
-                             All functions are defined in this file
-    =============================================================================================
+===============================================================================
+                 Common functions are defined here
+===============================================================================
 """
 
 pat_Chinese = re.compile(u'[\u2E80-\u9FFF]')
-pat_allSymbols = re.compile(u'[，。、？:;‘’“”…—！·《》【】{}/-|]')
-pat_commonSymbols = re.compile(u'[，。、？:;‘’“”…—！·-]')
-
-###
+pat_allSymbols = re.compile(u'[，。、？：；‘’“”…—！·《》【】{}/-|]')
+pat_commonSymbols = re.compile(u'[，。、？：；‘’“”…—！·-]')
 
 def get_source_txt(line):
     temp = line.strip('\n')
@@ -23,9 +28,9 @@ def get_source_txt(line):
     return source_txt
 
 
-def get_translation(source_txt,usr_input):
-   if usr_input:
-        translation = '|'.join((source_txt[0],usr_input))
+def get_translation(source_txt, raw_translation):
+   if raw_translation:
+        translation = '|'.join((source_txt[0], raw_translation))
         translation = ''.join((translation,'\n'))
         return translation
 
@@ -37,33 +42,19 @@ def add_space(usr_input):
             c = tmp.pop()
             if c != ' ':
                 tmp.append(c)
-            else:
-                pass
             tmp.append(char)
             if re.search(pat_commonSymbols,char):
                 tmp.append(' ')
         elif re.search(pat_Chinese,char):
             tmp.append(char)
-            tmp.append(' ')              # can't combine two statments because of pop() above
+            tmp.append(' ')  #can't combine 2 statments because of pop()
         else:
             tmp.append(char)
     usr_input_space = ''.join(tmp)
     return usr_input_space
 
-
-def get_offset(offset_file):
-    offset = 0
-    try:
-        offset_str = pickle.load(offset_file)
-    except EOFError:
-        pass
-    else:
-        if offset_str:
-            offset = int(offset_str)
-    return offset
-
-def cur_time():
-	t = datetime.time(1,2,3)
-	d = datetime.date.today()
-	cur_time = datetime.datetime.combine(d,t)
-	return cur_time 
+# def cur_time():
+# 	t = datetime.time(1,2,3)
+# 	d = datetime.date.today()
+# 	cur_time = datetime.datetime.combine(d,t)
+# 	return cur_time 
